31aec21374c7a28d8fd67c313273fdb26ef37019
WojciechKrzyzanowski4 <spider.wojciech@gmail.com>
2023-12-01 16:40:28 +0100

    current user retrival

--- src/main/java/com/Wkrzyz/HouseHoldIncomeManager/controller/AuthController.java
+++ src/main/java/com/Wkrzyz/HouseHoldIncomeManager/controller/AuthController.java
@@ -7,6 +7,8 @@ import com.Wkrzyz.HouseHoldIncomeManager.services.UserService;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -53,9 +55,27 @@ public class AuthController {
         //System.out.println(users);
         return "users";
     }
+    /**
+     * this function is an example how to get the data regarding the currently logged in user
+     * */
     @GetMapping("/adminPage")
-    public String testUser(Neo4jProperties.Authentication authentication){
-        System.out.println(authentication);
+    public String testUser(Neo4jProperties.Authentication authentication, Model model){
+        //getting the context of the currently logged in user
+        SecurityContext context = SecurityContextHolder.getContext();
+        //using the context to retrive the email of the currently logged in user
+        System.out.println(context.getAuthentication().getName());
+        System.out.println(context);
+
+        User user = userService.findUserByEmail(context.getAuthentication().getName());
+        //zaimplementowac konstruktor kopiujący
+        UserDto userDto = new UserDto(user);
+        Set<Role> roles = userDto.getRoles();
+        for(Role r : roles){
+            //we retrive a set from the database according to the found email
+            System.out.println(r);
+            //to do
+            //add this data to a return model for the controller
+        }
         return "_core";
     }
 
