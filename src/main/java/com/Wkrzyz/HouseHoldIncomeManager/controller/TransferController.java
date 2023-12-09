9ba3364edb0f12f3a9c7bed48d2242f57fff3e1f
WojciechKrzyzanowski4 <spider.wojciech@gmail.com>
2023-12-09 18:00:15 +0100

    controller changes

--- src/main/java/com/Wkrzyz/HouseHoldIncomeManager/controller/TransferController.java
+++ src/main/java/com/Wkrzyz/HouseHoldIncomeManager/controller/TransferController.java
@@ -3,6 +3,7 @@ package com.Wkrzyz.HouseHoldIncomeManager.controller;
 import com.Wkrzyz.HouseHoldIncomeManager.model.Transfer;
 import com.Wkrzyz.HouseHoldIncomeManager.model.User;
 import com.Wkrzyz.HouseHoldIncomeManager.model.dto.TransferDto;
+import com.Wkrzyz.HouseHoldIncomeManager.model.dto.UserDto;
 import com.Wkrzyz.HouseHoldIncomeManager.services.TransferService;
 import com.Wkrzyz.HouseHoldIncomeManager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -45,6 +48,15 @@ public class TransferController {
         model.addAttribute("user", user);
         List<TransferDto> transfers = transferService.findAll();
         model.addAttribute("transfers", transfers);
+        return "transfers";
+    }
+
+    @PostMapping("/transfer/save")
+    public String saveTransfer(@ModelAttribute("transfer") TransferDto transferDto, Model model){
+
+        //handle the save method here
+        //probably return the same page as the one we use to save the transfer but this reamains to be seen
+
         return "transfers";
     }
 }
