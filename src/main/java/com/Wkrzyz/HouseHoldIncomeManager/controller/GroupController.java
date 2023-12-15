b3f530b68fb63914106ea49fb9a626a7214fdf1c
WojciechKrzyzanowski4 <spider.wojciech@gmail.com>
2023-12-15 05:10:14 +0100

    template for group user add

--- src/main/java/com/Wkrzyz/HouseHoldIncomeManager/controller/GroupController.java
+++ src/main/java/com/Wkrzyz/HouseHoldIncomeManager/controller/GroupController.java
@@ -1,5 +1,8 @@
 package com.Wkrzyz.HouseHoldIncomeManager.controller;
 
+import com.Wkrzyz.HouseHoldIncomeManager.enums.Role;
+import com.Wkrzyz.HouseHoldIncomeManager.model.User;
+import com.Wkrzyz.HouseHoldIncomeManager.model.dto.TransferDto;
 import com.Wkrzyz.HouseHoldIncomeManager.model.dto.UserDto;
 import com.Wkrzyz.HouseHoldIncomeManager.services.TransferService;
 import com.Wkrzyz.HouseHoldIncomeManager.services.UserGroupService;
@@ -7,9 +10,15 @@ import com.Wkrzyz.HouseHoldIncomeManager.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class GroupController {
@@ -28,8 +37,23 @@ public class GroupController {
     }
 
 
-    //this controller will handle returning the main Group page data
-    //including:
-    //group specific transfers list
-    //
+    @GetMapping("/adduser")
+    public String addUser(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "adduser";
+    }
+
+
+    @PostMapping("/adduser/save")
+    public String registration( @ModelAttribute("user") UserDto userDto,
+                                BindingResult result,
+                                Model model){
+
+        System.out.println(userDto.getEmail());
+
+        //userService.saveUser(userDto);
+        return "redirect:/adduser?success";
+    }
+
 }
