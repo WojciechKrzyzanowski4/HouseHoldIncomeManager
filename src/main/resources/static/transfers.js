403b0c87d969fe3cd0317602a864f3f0a29f2a92
WojciechKrzyzanowski4 <spider.wojciech@gmail.com>
2024-01-09 20:08:01 +0100

    js is js my boi

--- src/main/resources/static/transfers.js
+++ src/main/resources/static/transfers.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+
     // Get data from the table
+    var currentDataScope = false;
     var table = document.getElementById('data-table');
+
     var labels = new Set();
     var data = [];
+
+    var currentLabels = [];
+    var currentData = [];
 
     for (var i = 1; i < table.rows.length; i++) {
         labels.add(table.rows[i].cells[2].innerText);
@@ -16,21 +23,26 @@ document.addEventListener('DOMContentLoaded', function () {
         var category = table.rows[i].cells[2].innerText;
         var value = parseFloat(table.rows[i].cells[1].innerText);
         var index = Array.from(labels).indexOf(category);
-        data[index] += value;
+        if(value > 0){
+            data[index] += value;
+        }
     }
-
-    console.log(data);
-
+    for (var i = 0; i < labels.size; i++) {
+        if(data[i] != 0){
+            currentLabels.push(Array.from(labels)[i]);
+            currentData.push(data[i]);
+        }
+    }
     // Render the pie chart
     var canvas = document.getElementById('myCategoryChart');
     var ctx = canvas.getContext('2d');
     var myCategoryChart = new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: Array.from(labels),
+            labels: currentLabels,
             datasets: [{
                 label: 'Values',
-                data: data,
+                data: currentData,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.8)',
                     'rgba(54, 162, 235, 0.8)',
@@ -47,9 +59,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
+
+
+    //listeners section
     // Get the button element
     var toggleChartButton = document.getElementById('toggleChartButton');
-
     // Add a click event listener to the button
     toggleChartButton.addEventListener('click', function () {
         // Toggle between different chart types
@@ -58,36 +73,88 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Update the chart type
         myCategoryChart.config.type = newType;
-
         myCategoryChart.options = getDefaultOptions(newType);
         // Update the chart
         myCategoryChart.update();
     });
-    //this sets the different styles of the chart depending of the button toggle
-     function getDefaultOptions(chartType) {
-            if (chartType === 'pie') {
-                return {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                };
-            } else if (chartType === 'bar') {
-                return {
 
-                    //you could put the code for changing the data format here
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        x: {
-                            beginAtZero: true,
-                        },
-                        y: {
-                            beginAtZero: true,
-                        },
-                    },
-                };
-            }
-            // We can also change the data and so on using methods like this
-            // Add more options as needed for other chart types
-            return {};
+    var toggleChartButtonDos = document.getElementById('SwitchChartButton');
+
+    toggleChartButtonDos.addEventListener('click', function () {
+        if(!currentDataScope){
+            myCategoryChart.data = getNeededData(labels, -1);
+            currentDataScope = !currentDataScope;
+        }else{
+            myCategoryChart.data = getNeededData(labels, 1);
+            currentDataScope = !currentDataScope;
         }
+        // Update the chart
+        myCategoryChart.update();
+    });
+
+
+    //functions section
+     function getDefaultOptions(chartType) {
+        if (chartType === 'pie') {
+            return {
+                responsive: true,
+                maintainAspectRatio: false,
+            };
+        } else if (chartType === 'bar') {
+            return {
+                //you could put the code for changing the data format here
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                    },
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            };
+        }
+        // We can also change the data and so on using methods like this
+        // Add more options as needed for other chart types
+        return {};
+    }
+
+     function getNeededData(labels, positive) {
+        var newData = [];
+        var newLabels = [];
+        var newCurrentData = [];
+        for (var i = 0; i < labels.size; i++) {
+            newData.push(0);
+        }
+        for (var i = 1; i < table.rows.length; i++) {
+            var category = table.rows[i].cells[2].innerText;
+            var value = parseFloat(table.rows[i].cells[1].innerText);
+            var index = Array.from(labels).indexOf(category);
+            if(value * positive > 0){
+                newData[index] += value;
+            }
+        }
+        for (var i = 0; i < labels.size; i++) {
+            if(newData[i] != 0){
+                newLabels.push(Array.from(labels)[i]);
+                newCurrentData.push(newData[i])
+            }
+        }
+        return {
+            labels: newLabels,
+            datasets: [{
+                label: 'Values',
+                data: newCurrentData,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 206, 86, 0.8)',
+                    // Add more colors as needed
+                ],
+                borderColor: 'rgba(0, 0, 0, 1)', // Border color
+                borderWidth: 1
+            }]
+        };
+    }
 });
