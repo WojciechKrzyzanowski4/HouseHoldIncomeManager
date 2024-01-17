ecadea4e48251b16cc9219483f84f6cc5ec7cd43
WojciechKrzyzanowski4 <spider.wojciech@gmail.com>
2024-01-17 02:56:16 +0100

    Removed Unused Files

--- src/main/java/com/Wkrzyz/HouseHoldIncomeManager/services/impl/UserGroupServiceImpl.java
+++ src/main/java/com/Wkrzyz/HouseHoldIncomeManager/services/impl/UserGroupServiceImpl.java
@@ -1,24 +1,20 @@
 package com.Wkrzyz.HouseHoldIncomeManager.services.impl;
 
-import com.Wkrzyz.HouseHoldIncomeManager.model.User;
 import com.Wkrzyz.HouseHoldIncomeManager.model.UserGroup;
 import com.Wkrzyz.HouseHoldIncomeManager.model.dto.UserGroupDto;
 import com.Wkrzyz.HouseHoldIncomeManager.repository.UserGroupRepository;
-import com.Wkrzyz.HouseHoldIncomeManager.repository.UserRepository;
 import com.Wkrzyz.HouseHoldIncomeManager.services.UserGroupService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserGroupServiceImpl implements UserGroupService {
-
-    private UserRepository userRepository;
+    
 
     private UserGroupRepository userGroupRepository;
 
 
-    public UserGroupServiceImpl(UserGroupRepository userGroupRepository, UserRepository userRepository ) {
-        this.userRepository = userRepository;
+    public UserGroupServiceImpl(UserGroupRepository userGroupRepository ) {
+        
         this.userGroupRepository = userGroupRepository;
     }
     @Override
